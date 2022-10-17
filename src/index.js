b7eca9f7f7c40a3c0910d7354c5a18cd91c6c604
Sebastian Caicedo <jsebdev@gmail.com>
2022-10-17 19:42:40 +0200

    Add ScrollToTop component

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import App from './ui/App/App';
 import reportWebVitals from './reportWebVitals';
 import { persistor, store } from './store';
 import { PersistGate } from 'redux-persist/integration/react';
-// import { HashRouter as Router } from 'react-router-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ScrollToTop } from 'ScrollToTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,6 +14,7 @@ root.render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <Router>
+          <ScrollToTop />
           <App />
         </Router>
       </PersistGate>
